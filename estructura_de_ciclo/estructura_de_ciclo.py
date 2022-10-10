b27c1a882cb3305c029a592706f602b4293d2719
kevin <kevinriverasante@gmail.com>
2022-10-10 10:17:40 -0500

    clases del dia lunes

--- estructura_de_ciclo/estructura_de_ciclo.py
+++ estructura_de_ciclo/estructura_de_ciclo.py
@@ -36,3 +36,8 @@ while condicion==True:
     condicion=False
   else:
     print("ese no es el numero") 
+##crear un programa que me pida mi edad
+#si engreso una edad incorrecta el programa
+#seguira pidiendo mi edad
+##si es la edad corrceta me mostrara un mensaje de correcto
+#y se terminara la ejecucion
--- tarea01/tarea01.py
+++
@@ -1,7 +0,0 @@
-##crear un programa que me pida mi edad
-#si engreso una edad incorrecta el programa
-#seguira pidiendo mi edad
-##si es la edad corrceta me mostrara un mensaje de correcto
-#y se terminara la ejecucion
-
-#averiguar sobre funciones
