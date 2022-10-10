e14f57319a0645f28a33f842ee21333f904e5fd4
kevin <kevinriverasante@gmail.com>
2022-10-10 10:15:04 -0500

    clase del dia lunes

---
+++ estructura_de_ciclo/estructura_de_ciclo.py
@@ -0,0 +1,38 @@
+#ESTRUCTURA DE CICLO
+ #permite ejecutar funciones varias veces, estos son utilizados 
+ #para determinar de una forma denamica en lugar de agregar la funcion n numeros de veces
+ 
+ #for 
+ #se inicializar variable que sera el contador, solo pasa por aqui al iniciar el ciclo var a =0;
+ #se determina el numero de veces que se ejecutara el bloque de codigo a< 10;
+ #se ejecutara cada vez que termine el bloque de codigo (mayormente se utiliza para aumentar o disminuir segun lo que se quiere hacer)
+ 
+ #ejemplo1
+ #for (var a = 0; a < 10; a++)
+ #{
+    #//bloque de codigo
+    #console.log(a + "hola amigo");
+ #}
+
+ #ejemplo2
+ #var x = 0, medida;
+ #var dias= ["L", "M", "Mi", "J", "V"];
+ #medida = dias.length;
+   #for(; x < medida; x++)
+ # {
+    #console.log(dias[x]);
+  #}
+ #while tambien es estructura de ciclo
+
+ #int es un metodo de python que transforma un dato
+ #tipo texto a un dato de tipo numerico real o entero
+ # input es un metodo de python que pide un dato
+ #por consola al usuario
+condicion=True
+while condicion==True:
+  numero=int(input("ingresa el numero ganador:"))
+  if numero==10:
+    print("ganaste el premio")
+    condicion=False
+  else:
+    print("ese no es el numero") 
---
+++ estructura_secuencial/estructura_secuencial.py
@@ -0,0 +1,13 @@
+#ESTRUCTURA SECUENCIAL
+
+#1:DECLARACION O DEFINICION DE VARIABLES: se enlistan al principio del algoritnmo todas las varibles que se uzaran, 
+#colocando el nombre y el tipo.
+
+#2:INICIALIZACION DE VARIABLES: asignar agun valor,puede ser numerio,logico o de otro tip.
+
+#3:LECTURA O ENTRADA DE DATOS: el dato se recibe desde un despositivo de entrada y se almacena en la variable que aparece
+#despues de la instruccion.
+
+#4:CALCULOS: los valores de entrada se realizaran las operaciones para indicar la salida
+
+#5:ESCRITURA O SALIDA DE DATOS: el resultado se envia a traves de un dispositivo de salida y la instruccion se escribe entre comillas .
