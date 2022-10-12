5b92e916862d79c79b572d043606a0bbdde5ba42
kevin <kevinriverasante@gmail.com>
2022-10-12 10:59:34 -0500

    mi trabajo hecho en casa

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,1 +1,21 @@
 #averiguar sobre funciones
+#líneas de código que realizan una tarea específica y pueden tomar
+#“Argumentos” para diferentes “Parámetros” que modifiquen su funcionamiento y los datos de salida. 
+def multiplica_por_7(numero):
+    print(f'{numero}*7 = {numero * 7}')
+print("comienzo del programa ")
+multiplica_por_7(7)
+print("seguiente")
+multiplica_por_7(113)
+print("fin")
+##la funcion multiplica_por_7() define un parametro llamado numero que es lo que se uteliza para multiplicar por 5
+##el resultado del programa anterior seria el siguiente:
+#comienzo del programa
+ #7*7 = 49
+ #seguiente
+ #113*7 = 791
+ #fin
+#el programa comienza su ejecucion en la line 6 y va ejecutando las instrucciones una a una de manera ordenada
+#cuando se encuentra el nokmbre de la funcion "multiplica_por_7(), el flujo de ejecucion pasa a la primera instruccion
+#de la funcion.cuando se llega a la instruccion de la funcion; el flujo del programa sigue por la instruccion que hay a continuacion
+#de la llamada de funcion.
